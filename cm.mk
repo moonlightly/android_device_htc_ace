aa95c583da16727c495d17176d6f8624ff67d8b9
synergydev <synergy@spentlemon.com>
2012-11-18 18:56:25 +0000

    4.2 updates

--- cm.mk
+++ cm.mk
@@ -9,10 +9,7 @@ TARGET_BOOTANIMATION_NAME := 480
 # Inherit CM stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit AOSP device configuration for ace
-$(call inherit-product, device/htc/ace/device_ace.mk)
-
-PRODUCT_NAME := cm_ace
+PRODUCT_NAME := full_ace
 PRODUCT_DEVICE := ace
 PRODUCT_BRAND := htc_wwe
 PRODUCT_MODEL := Desire HD
